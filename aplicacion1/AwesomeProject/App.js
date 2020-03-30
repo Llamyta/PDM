435de61cc6dce54662d0af5202ee54fc931be284
llamyta <mbernardaserrano@gmail.com>
2020-03-29 22:58:51 -0400

    boton

--- aplicacion1/AwesomeProject/App.js
+++ aplicacion1/AwesomeProject/App.js
@@ -11,11 +11,16 @@ import {StyleSheet, View, TextInput} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MyComponent from './src/test/MyComponent';
-import Logo from './src/login/Logo';
-import User from "./src/image/username.png"
 
+import User from "./src/image/username.png"
 import Pass from './src/image/password.png';
+
+import Logo from './src/login/Logo';
 import Input from './src/login/Input';
+import Button from './src/login/Button';
+
+const PASS=1;
+const USER=2;
 
 const App: () => React$Node = () => {
   const [value, onChangeText] = React.useState('Useless Placeholder');
@@ -30,14 +35,16 @@ const App: () => React$Node = () => {
           placeholder="Username"
           secureTextEntry={false}
           autoCorrect={false}
+          onChangeText={USER=>this.setState({USER})}
         />
          <Input
           source={Pass}
           placeholder="Password"
           secureTextEntry={false}
           autoCorrect={false}
+          onChangeText={PASS=>this.setState({PASS})}
         />
-        
+        <Button pass={USER} user={PASS} style={styles.button}/>
       </View>
     </View>
   </>
@@ -45,11 +52,21 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
+  //rosado f1414e
+  //rosado claro f77e79
+  //verde 4cbcb3
+  //verde claro 9cdfd4
+  //verde feo 70ffa2
+  //plomo 2e2e2e
+  //plomo claro basico c8c8c8
+  //plomo claro basico 2 e1e0de
+  //blanco f2f2f2
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: Colors.primary,
+    backgroundColor: '#4cbcb3',
   },
+
   container: {
     flex: 0.5,
     flexDirection: 'column',
@@ -58,6 +75,7 @@ const styles = StyleSheet.create({
   box: {
     height: 100,
   },
+
 
 });
 
---
+++ aplicacion1/AwesomeProject/src/login/constants.js
@@ -0,0 +1,7 @@
+
+export default {
+    PASS:"Password",
+    USERNAME:"Username",
+    DEFAULT_MARGIN_BURRON:100,
+    TITLE_BUTTON:"Ingresar"
+}
